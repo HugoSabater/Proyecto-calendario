c125a5dc256d27097da157a243e837026b1f5708
juliovi <86148299+juliovi10@users.noreply.github.com>
2025-12-05 10:33:40 +0100

    Delete agenda-de-actividades-fisicas-dockerized directory

--- agenda-de-actividades-fisicas-dockerized/components/icons/LightningBoltIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const LightningBoltIcon: React.FC = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-cyan-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
-    </svg>
-);
